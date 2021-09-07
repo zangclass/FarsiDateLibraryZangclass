529a69587ec3ee1eeabd117cb665fe9b0a268f37
Ebrahim Rt <ebrahim.rayatparvar@hotmail.com>
2021-09-07 14:30:16 +0430

    ساخت اینام ماهای شمسی

---
+++ FarsiDateLibraryZangclass/MonthFarsi.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarsiDateLibraryZangclass
+{
+    public enum MonthFarsi
+    {
+        [Display(Name ="قروردین")]
+        Farvardin,
+        [Display(Name = "اردیبهست")]
+        Ordibehesht,
+        [Display(Name = "خرداد")]
+        Khordad,
+        [Display(Name = "تیر")]
+        Tir,
+        [Display(Name = "مرداد")]
+        Mordad,
+        [Display(Name = "شهریور")]
+        Shahrivar,
+        [Display(Name = "مهر")]
+        Mehr,
+        [Display(Name = "آبان")]
+        Aban,
+        [Display(Name = "آذر")]
+        Azar,
+        [Display(Name = "دی")]
+        Dey,
+        [Display(Name = "بهمن")]
+        Bahman,
+        [Display(Name = "اسفند")]
+        Esfand
+    }
+}
