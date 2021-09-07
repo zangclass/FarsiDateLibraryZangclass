8cfdc1413d28a32a8bba98846a54b7648aad3b2a
Ebrahim Rt <ebrahim.rayatparvar@hotmail.com>
2021-09-07 14:34:59 +0430

    دریافت تاریخ و ساعت حال به صورت شمسی

--- FarsiDateLibraryZangclass/FarsiDateZangclass.cs
+++ FarsiDateLibraryZangclass/FarsiDateZangclass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace FarsiDateLibraryZangclass
 {
     public static class FarsiDateZangclass
     {
+        public static string Now()
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return $"{pc.GetYear(DateTime.Now)}/{pc.GetMonth(DateTime.Now)}" +
+                $"/{pc.GetDayOfWeek(DateTime.Now)} {DateTime.Now.ToLongTimeString()}";
+        }
     }
+
 }
