ded34cdfaaef4fee4f3a95a464dc0bf0a76b7ee7
Ebrahim Rt <ebrahim.rayatparvar@hotmail.com>
2021-09-07 14:25:26 +0430

    ساخت اینام برای روز های هفته به فارسی و انگلیسی

---
+++ FarsiDateLibraryZangclass/EnglishDay.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarsiDateLibraryZangclass
+{
+    public enum EnglishDay
+    {
+        [Display(Name = "شنبه")]
+        Saturday,
+        [Display(Name = "یکشنبه")]
+        Sunday,
+        [Display(Name = "دوشنبه")]
+        Monday,
+        [Display(Name = "سه شنبه")]
+        Tuesday,
+        [Display(Name = "چهار شنبه")]
+        Wednesday,
+        [Display(Name = "پنج شنبه")]
+        Thursday,
+        [Display(Name = "جمعه")]
+        Friday
+    }
+}
---
+++ FarsiDateLibraryZangclass/FarsiDay.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarsiDateLibraryZangclass
+{
+    public enum FarsiDay
+    {
+        [Display(Name = "شنبه")]
+        Shanbeh,
+        [Display(Name = "یکشنبه")]
+        YekShanbeh,
+        [Display(Name = "دوشنبه")]
+        Doshanbeh,
+        [Display(Name = "سه شنبه")]
+        SeShanbe,
+        [Display(Name = "چهار شنبه")]
+        ChaharShanbe,
+        [Display(Name = "پنج شنبه")]
+        PanjShanbeh,
+        [Display(Name = "جمعه")]
+        jomee
+    }
+}
