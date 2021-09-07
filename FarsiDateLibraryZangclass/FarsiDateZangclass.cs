3a78cda97cb45330832bdbb5e03a04e405a327d0
Ebrahim Rt <ebrahim.rayatparvar@hotmail.com>
2021-09-07 16:19:55 +0430

    گرفتن تاریخ میلادی و تبدیل به تاریخ شمسی

--- FarsiDateLibraryZangclass/FarsiDateZangclass.cs
+++ FarsiDateLibraryZangclass/FarsiDateZangclass.cs
@@ -15,6 +15,18 @@ namespace FarsiDateLibraryZangclass
             return $"{pc.GetYear(DateTime.Now)}/{pc.GetMonth(DateTime.Now)}" +
                 $"/{pc.GetDayOfWeek(DateTime.Now)} {DateTime.Now.ToLongTimeString()}";
         }
+
+        /// <summary>
+        /// تبدیل تاریخ میلادی به شمسی
+        /// </summary>
+        /// <param name="date">ورود تاریخ میلادی</param>
+        /// <returns>خروجی تاریخ شمسی</returns>
+        public static string ConvertDateTo(DateTime date)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return $"{pc.GetYear(date)}/{pc.GetMonth(date)}" +
+                $"/{pc.GetDayOfWeek(date)} {date.ToLongTimeString()}";
+        }
     }
 
 }
