43fbc6c0470303c6332e5f4abcec619297b47ddc
Ebrahim Rt <ebrahim.rayatparvar@hotmail.com>
2021-09-07 16:30:59 +0430

    پیاده سازی تابع گرفتن تاریخ شمسی و تبدیل به تاریخ میلادی

--- FarsiDateLibraryZangclass/FarsiDateZangclass.cs
+++ FarsiDateLibraryZangclass/FarsiDateZangclass.cs
@@ -9,11 +9,19 @@ namespace FarsiDateLibraryZangclass
 {
     public static class FarsiDateZangclass
     {
+        public static string Year { get; set; }
+        public static string Month { get; set; }
+        public static string Day { get; set; }
+        /// <summary>
+        /// تاریخ شمسی همان لحظه در سیستم را به شما باز میگرداند
+        /// </summary>
+        /// <returns>خروجی تاریخ شمسی است</returns>
         public static string Now()
         {
             PersianCalendar pc = new PersianCalendar();
-            return $"{pc.GetYear(DateTime.Now)}/{pc.GetMonth(DateTime.Now)}" +
+            var str = $"{pc.GetYear(DateTime.Now)}/{pc.GetMonth(DateTime.Now)}" +
                 $"/{pc.GetDayOfWeek(DateTime.Now)} {DateTime.Now.ToLongTimeString()}";
+            return str;
         }
 
         /// <summary>
@@ -26,6 +34,20 @@ namespace FarsiDateLibraryZangclass
             PersianCalendar pc = new PersianCalendar();
             return $"{pc.GetYear(date)}/{pc.GetMonth(date)}" +
                 $"/{pc.GetDayOfWeek(date)} {date.ToLongTimeString()}";
+        }
+
+        /// <summary>
+        /// تبدیل تاریخ شمسی به تاریخ میلادی
+        /// </summary>
+        /// <param name="yaer">سال ورودی</param>
+        /// <param name="month">ماه ورودی</param>
+        /// <param name="day">روز ورودی</param>
+        /// <returns>خروجی ماه میلادی به صورت رشته می باشد</returns>
+        public static string ConvertDateTo(int yaer,int month,int day)
+        {
+            PersianCalendar p = new PersianCalendar();
+            string str = Convert.ToString(p.ToDateTime(yaer, month, day, 0, 0, 0, 0), CultureInfo.InvariantCulture);
+            return str.Substring(0, 10);
         }
     }
 
